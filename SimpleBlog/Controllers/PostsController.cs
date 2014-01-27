7baed5318f99bc75463df9d42345808e013e29fd
Maikel Ninaber <maikel.ninaber@capgemini.com>
2014-01-27 12:10:22 +0100

    Added basic routes and controllers and the admin area

---
+++ SimpleBlog/Controllers/PostsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Controllers
+{
+    public class PostsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Hello world");
+        }
+
+
+
+
+
+    }
+}
